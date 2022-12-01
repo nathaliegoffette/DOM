8d736166f13c3e48aeeb22d31e0a030da9ea1550
Nathalie Goffette <nathaliegoffette@gmail.com>
2022-12-01 14:37:52 +0100

    js terminé

--- 2. Collection/script.js
+++ 2. Collection/script.js
@@ -1,109 +1,165 @@
+
+
 const collection = [
     {
-    name : "Send me on my way",
+    title : "Send me on my way",
     singer : "Rusted Root",
     releaseYear : 1994,
-    genre : "Roots Rock",
+    style : "Roots Rock",
     label : "Warner Records",
-    picture : "./assets/Rusted_Root_-_Send_Me_on_My_Way.png/",
+    picture : "./assets/send-me-on-my-way.jpeg",
     },
 
     {
-    name : "Little Bitty Pretty One",
+    title : "Little Bitty Pretty One",
     singer : "Thurston Harris",
     releaseYear : 1957,
-    genre :"Rock'n roll", 
+    style :"Rock'n roll", 
     label : "Aladdin",
     picture :"./assets/littleBittyPrettyOne.jpeg",
     },
 
     {
-    name : "One Way or Another",
+    title : "One Way or Another",
     singer : "Blondie",
     releaseYear : 1978,
-    genre : "Rock",
+    style : "Rock",
     label : "Chrysalis\EMI Records (USA)",
     picture : "./assets/blondie-one-way-or-another.jpg",
     },
 
     {
-    name : "Didn't Want to Be This Lonely",
+    title : "Didn't Want to Be This Lonely",
     singer : "Pretenders",
     releaseYear : 2020,
-    genre : "Rock",
+    style : "Rock",
     label : "BMG Rights Management",
     picture :"./assets/DidntWantToBeThisLonely.jpeg",
     },
 
     {
-    name : "Lonely Boy",
+    title : "Lonely Boy",
     singer : "The Black Keys",
     releaseYear : 2011,
-    genre : "Rock",
+    style : "Rock",
     label : "Wixen Music Publishing, Inc.",
     picture :"./assets/The-Black-Keys-Lonely-Boy.jpg",
     },
 
     {
-    name : "Pressure",
+    title : "Pressure",
     singer : "Billy Joel",
     releaseYear : 1982,
-    genre : "Classic Rock",
+    style : "Classic Rock",
     label : "Columbia",
     picture :"./assets/PressureBillyJoel.jpg",
     },
 
     {
-    name :"Country House",
+    title :"Country House",
     singer :"Blur",
     releaseYear : 1995,
-    genre : "Rock",
+    style : "Rock",
     label :"Parlophone UK",
     picture :"./assets/CountryHouse.jpeg",
     },
 
     {
-    name :"Got My Mind Set On You",
+    title :"Got My Mind Set On You",
     singer :"George Harrison",
     releaseYear : 1987,
-    genre : "Rock Folk",
+    style : "Rock Folk",
     label :"Parlophone",
     picture :"./assets/gotMyMindSetOnYou.jpg",
     },
 
     {
-    name :"Handle With Care",
+    title :"Handle With Care",
     singer :"Travaling Wilsbury",
     releaseYear : 1988,
-    genre : "Folk Rock",
+    style : "Folk Rock",
     label :"Concord Records",
     picture :"./assets/Handle_with_Care_Single.jpg",
     },
 
     {
-    name :"Livin'On The Edge",
+    title :"Livin'On The Edge",
     singer :"Aerosmith",
     releaseYear : 1993,
-    genre : "Rock",
+    style : "Rock",
     label :"Geffen",
     picture :"./assets/LivinOnTheEdge.jpeg",
     },
 
     {
-    name :"Bigmouth Strikes Again - 2011 Remastered",
+    title :"Bigmouth Strikes Again - 2011 Remastered",
     singer :"The Smiths",
     releaseYear : 1986,
-    genre : "Pop Rock",
+    style : "Pop Rock",
     label :"WM UK",
     picture :"./assets/BigmouthStrikesAgain.jpeg",
     },
 
     {
-    name :"Gimme Dat Ding",
+    title :"Gimme Dat Ding",
     singer :"The Pipkins",
     releaseYear : 1970,
-    genre : "Pop",
+    style : "Pop",
     label :"Chrysalis Copyrights",
     picture :"./assets/GimmeDatDing.jpeg",
     },
 ]
+
+let htmlCode = ``;
+
+
+collection.forEach(function(singleSongObjects) {
+
+  htmlCode =
+    htmlCode +
+    `
+    <article>
+      <div>
+      <img src="${singleSongObjects.picture}">
+      </div>
+      <div>
+      <h3>Title: ${singleSongObjects.title}</h3>
+      <p>Singer: ${singleSongObjects.singer}</p>
+      <p>Style: ${singleSongObjects.style}</p>
+      <p>Label: ${singleSongObjects.label}</p>
+      <p>Year of release: ${singleSongObjects.releaseYear}</p>
+      </div>
+    </article>
+  `;
+});
+
+const songCards = document.querySelector(".all-songs-cards");
+
+songCards.innerHTML = htmlCode;
+
+/*let body = document.querySelector('body');
+
+let container = document.createElement("div");
+    container.className = "container";
+
+body.append(container);
+
+for(let i=0; i<collection.length;i++){
+    const song = document.createElement("div")
+    song.className = "song"
+
+    const list = document.createElement("p")
+
+    list.innerText = `${collection[i].title} ${collection[i].singer} ${collection[i].releaseYear} ${collection[i].style} ${collection[i].label} ${collection[i].picture}`
+    song.appendChild(list)
+
+    let container = document.createElement("div");
+    container.className = "container"
+    
+    song.appendChild(list);
+
+    container.appendChild(song);
+
+    
+}
+*/
