01e40f681c11fa972e58d8192cc314a10a996fa1
Nathalie Goffette <nathaliegoffette@gmail.com>
2022-12-01 10:29:24 +0100

    Array JS

---
+++ 2. Collection/script.js
@@ -0,0 +1,109 @@
+const collection = [
+    {
+    name : "Send me on my way",
+    singer : "Rusted Root",
+    releaseYear : 1994,
+    genre : "Roots Rock",
+    label : "Warner Records",
+    picture : "./assets/Rusted_Root_-_Send_Me_on_My_Way.png/",
+    },
+
+    {
+    name : "Little Bitty Pretty One",
+    singer : "Thurston Harris",
+    releaseYear : 1957,
+    genre :"Rock'n roll", 
+    label : "Aladdin",
+    picture :"./assets/littleBittyPrettyOne.jpeg",
+    },
+
+    {
+    name : "One Way or Another",
+    singer : "Blondie",
+    releaseYear : 1978,
+    genre : "Rock",
+    label : "Chrysalis\EMI Records (USA)",
+    picture : "./assets/blondie-one-way-or-another.jpg",
+    },
+
+    {
+    name : "Didn't Want to Be This Lonely",
+    singer : "Pretenders",
+    releaseYear : 2020,
+    genre : "Rock",
+    label : "BMG Rights Management",
+    picture :"./assets/DidntWantToBeThisLonely.jpeg",
+    },
+
+    {
+    name : "Lonely Boy",
+    singer : "The Black Keys",
+    releaseYear : 2011,
+    genre : "Rock",
+    label : "Wixen Music Publishing, Inc.",
+    picture :"./assets/The-Black-Keys-Lonely-Boy.jpg",
+    },
+
+    {
+    name : "Pressure",
+    singer : "Billy Joel",
+    releaseYear : 1982,
+    genre : "Classic Rock",
+    label : "Columbia",
+    picture :"./assets/PressureBillyJoel.jpg",
+    },
+
+    {
+    name :"Country House",
+    singer :"Blur",
+    releaseYear : 1995,
+    genre : "Rock",
+    label :"Parlophone UK",
+    picture :"./assets/CountryHouse.jpeg",
+    },
+
+    {
+    name :"Got My Mind Set On You",
+    singer :"George Harrison",
+    releaseYear : 1987,
+    genre : "Rock Folk",
+    label :"Parlophone",
+    picture :"./assets/gotMyMindSetOnYou.jpg",
+    },
+
+    {
+    name :"Handle With Care",
+    singer :"Travaling Wilsbury",
+    releaseYear : 1988,
+    genre : "Folk Rock",
+    label :"Concord Records",
+    picture :"./assets/Handle_with_Care_Single.jpg",
+    },
+
+    {
+    name :"Livin'On The Edge",
+    singer :"Aerosmith",
+    releaseYear : 1993,
+    genre : "Rock",
+    label :"Geffen",
+    picture :"./assets/LivinOnTheEdge.jpeg",
+    },
+
+    {
+    name :"Bigmouth Strikes Again - 2011 Remastered",
+    singer :"The Smiths",
+    releaseYear : 1986,
+    genre : "Pop Rock",
+    label :"WM UK",
+    picture :"./assets/BigmouthStrikesAgain.jpeg",
+    },
+
+    {
+    name :"Gimme Dat Ding",
+    singer :"The Pipkins",
+    releaseYear : 1970,
+    genre : "Pop",
+    label :"Chrysalis Copyrights",
+    picture :"./assets/GimmeDatDing.jpeg",
+    },
+]
